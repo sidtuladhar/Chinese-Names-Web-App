580ee5a2eeaa8acb39bde7162783a99f63a3b250
sidtuladhar <smt9598@nyu.edu>
2023-09-06 17:44:46 -0400

    test 2

--- api/index.py
+++ api/index.py
@@ -16,24 +16,24 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/search', methods=['GET'])
-def search_names():
-    search_term = request.args.get('q', '').lower()
-    results = []
-
-    # Search through the name columns for matches
-    name_columns = [col for col in top100name_df.columns if col.startswith('name')]
-    for col in name_columns:
-        matches = top100name_df[top100name_df[col].str.lower().str.contains(search_term, na=False)]
-        if not matches.empty:
-            for _, row in matches.iterrows():
-                result = {
-                    'Name': row[col],
-                    'Year': col.split('.')[-1]
-                }
-                results.append(result)
-
-    return jsonify(results)
+# @app.route('/search', methods=['GET'])
+# def search_names():
+#     search_term = request.args.get('q', '').lower()
+#     results = []
+#
+#     # Search through the name columns for matches
+#     name_columns = [col for col in top100name_df.columns if col.startswith('name')]
+#     for col in name_columns:
+#         matches = top100name_df[top100name_df[col].str.lower().str.contains(search_term, na=False)]
+#         if not matches.empty:
+#             for _, row in matches.iterrows():
+#                 result = {
+#                     'Name': row[col],
+#                     'Year': col.split('.')[-1]
+#                 }
+#                 results.append(result)
+#
+#     return jsonify(results)
 
 
 @app.route('/about')
