cf1da7d39d6b471ab06ab33ca41ce8ffdafc0d8f
sidtuladhar <smt9598@nyu.edu>
2023-09-07 12:17:48 -0400

    test 4

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import pandas as pd
 
-app = Flask(__name__, template_folder='templates', static_folder='/static')
+app = Flask(__name__, template_folder='templates', static_folder='../static')
 
 top100name_df = pd.read_csv('../data/top100name.year.csv')
 top50char_df = pd.read_csv('../data/top50char.year.csv')
@@ -16,29 +16,29 @@ def index():
     return render_template('index.html')
 
 
-# @app.route('/search', methods=['GET'])
-# def search_names():
-#     search_term = request.args.get('q', '').lower()
-#     results = []
-#
-#     # Search through the name columns for matches
-#     name_columns = [col for col in top100name_df.columns if col.startswith('name')]
-#     for col in name_columns:
-#         matches = top100name_df[top100name_df[col].str.lower().str.contains(search_term, na=False)]
-#         if not matches.empty:
-#             for _, row in matches.iterrows():
-#                 result = {
-#                     'Name': row[col],
-#                     'Year': col.split('.')[-1]
-#                 }
-#                 results.append(result)
-#
-#     return jsonify(results)
-#
-#
-# @app.route('/about')
-# def about():
-#     return 'About'
+@app.route('/search', methods=['GET'])
+def search_names():
+    search_term = request.args.get('q', '').lower()
+    results = []
+
+    # Search through the name columns for matches
+    name_columns = [col for col in top100name_df.columns if col.startswith('name')]
+    for col in name_columns:
+        matches = top100name_df[top100name_df[col].str.lower().str.contains(search_term, na=False)]
+        if not matches.empty:
+            for _, row in matches.iterrows():
+                result = {
+                    'Name': row[col],
+                    'Year': col.split('.')[-1]
+                }
+                results.append(result)
+
+    return jsonify(results)
+
+
+@app.route('/about')
+def about():
+    return 'About'
 
 
 if __name__ == '__main__':
