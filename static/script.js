2f38e4109c15741d5d6336e8ade5dade9a534211
sidtuladhar <smt9598@nyu.edu>
2023-09-09 19:05:18 -0400

    feat: works as a flask app, not vercel

--- static/script.js
+++ static/script.js
@@ -1,5 +1,7 @@
 function searchNames() {
-    const searchInput = document.getElementById('searchInput').value;
+    const searchInput = document.getElementById('name').value;
+    console.log('Search Input:', searchInput);
+
     fetch(`/search?q=${encodeURIComponent(searchInput)}`)
         .then(response => response.json())
         .then(data => displayResults(data));
@@ -19,5 +21,19 @@ function displayResults(results) {
             resultList.appendChild(listItem);
         });
         searchResults.appendChild(resultList);
+    }
+}
+
+function validateAndSearch() {
+    const name = document.getElementById("name").value;
+    const confirmName = document.getElementById("confirm_name").value;
+
+    if (name !== confirmName) {
+        alert("Names do not match. Please check and try again.");
+    } else if (name === "" || confirmName === "") {
+        alert("Please enter a name.");
+    } else {
+        // Names match, initiate the search
+        searchNames();
     }
 }
